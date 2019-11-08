be4d9170411f867a46a6e321838d9514c9817892
cmcgowan209 <cmcgowan1999@gmail.com>
2019-11-08 14:17:59 +0000

    Reverted to a previous build, simplified code, attempted to fix error.

--- SecondIteration.java
+++ SecondIteration.java
@@ -1,40 +1,43 @@
-public class SecondIteration{
-    //argument to be passed in
-    private static int Arg1;
-    //use of a try catch to catch if the code is receiving a valid integer.
-    public static void main(String args[]) throws Exception {
-		if (args.length > 0) {
-			try{
-				//Find out if the number is an integer by parsing it, if not, throw an error
-				Arg1 = Integer.parseInt(args[0]);
-			} 
-			catch (NumberFormatException e){
-				//inform user that this input can't be used
-				System.err.println("Argument" + args[0] + " must be an integer.");
-				System.exit(1);
-			}
-	}
-		else  {
-			//ask the user to fix their mistake, close the program.
-			System.err.println("Please provide a suitable integer argument");
-			System.exit(0);      
-	}
-                
-	//create a array of all the characters used in hexadecimal notation. 
+class SecondIteration
+{
+
+    public static int Arg1;
+
+    public static void main(String args[])
+    {
+        if (args.length > 0) 
+        {
+            try 
+            {
+                Arg1 = Integer.parseInt(args[0]);
+            } 
+            catch (NumberFormatException e) 
+            {
+                System.err.println("Argument" + args[0] + " must be an integer.");
+                System.exit(1);
+            }
+        }else if (args.length == 0) {
+			System.err.println("No Input. Terminating session.");
+			System.exit(0);
+		} else {
+			System.err.println("Invalid input. Terminating session.");
+			System.exit(0);
+		}
+
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem; 
-        int num = 0;
-	String hex=""; 
-        System.out.println("Convertint to hexadecimal" + num + "To Hex...");
-        //calculation for creation of the hexadecimal
-        while(num != 0){
+         int rem, num;
+	num = Arg1;
+	String hexadecimal=""; 
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        
+        while(num != 0)
+        {
 	    rem=num%16;
-	    hex= ch[rem] + hex;
+	    hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
         
-        System.out.println("Hexadecimal representation is : " + hex);
-        
+        System.out.println("Hexadecimal representation is : " + hexadecimal);
 
-}
+    }
 }
