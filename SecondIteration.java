5fe7f5fd031544ee866ca8035515e8b687e90412
cmcgowan209 <cmcgowan1999@gmail.com>
2019-11-08 13:53:40 +0000

    Syntax error fixed. Misplaced curly brace.

--- SecondIteration.java
+++ SecondIteration.java
@@ -2,7 +2,7 @@ public class SecondIteration{
     //argument to be passed in
     private static int Arg1;
     //use of a try catch to catch if the code is recieving a valid integer.
-    public static void main(String args[]) throws Exception {
+    public static String main(String args[]) throws Exception {
 		if (args.length > 0) {
 			try{
 				//Find out if the number is an integer by parsing it, if not, throw an error
@@ -16,18 +16,16 @@ public class SecondIteration{
 	}
 		else if (args.length == 0) {
 			//ask the user to fix their mistake, close the program.
-			System.err.println("Please provide a suitable integer argument"0
+			System.err.println("Please provide a suitable integer argument");
 			System.exit(0);      
 	}
-        
-    }
+                
 	//create a array of all the characters used in hexadecimal notation. 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem; 
-        int num;
-	num = decimal;
+        int num = 0;
 	String hex=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        System.out.println("Convertint to hexadecimal" + num + "To Hex...");
         //calculation for creation of the hexadecimal
         while(num != 0){
 	    rem=num%16;
@@ -36,6 +34,6 @@ public class SecondIteration{
         }
         
         System.out.println("Hexadecimal representation is : " + hex);
-        return hex;
-       
+        return hex;   
+    }
 }
